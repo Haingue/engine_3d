bb93331a7ffb1d372c0b4eb2686a75bf0de1221e
haingue <fabien.haingue@gmail.com>
2023-10-29 21:17:50 +0100

    FEAT: add README and increase the frame speed

---
+++ README.md
@@ -0,0 +1,37 @@
+# Rust 3d Engine
+
+This is a simple project to practice 3d algorithms and practice Rust inspired by the youtube channel: [Quantale](https://www.youtube.com/watch?v=UkPTyojw7IA).
+
+## Getting started
+
+### Prerequisites
+
+Rust environment installed.
+
+```shell
+$ rustup check
+```
+
+### Installation
+
+```shell
+$ cargo build
+```
+
+### Run
+
+Without build
+
+```shell
+$ cargo run
+```
+
+With the build
+
+```shell
+$ .\target\debug\engine_3d.exe
+```
+
+## Licencing
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,24 @@ fn main() {
     let mut engine: Engine = Engine::new(configuration.width, configuration.height - 1);
     println!("Engine: {:?}", engine.to_string());
 
-    let point_1 = Vec2::new(1, 10);
-    let point_2 = Vec2::new(10, 1);
-    println!("Try point: {:?} & {:?}", point_1, point_2);
-    engine.clear(' ');
-    engine.put_pixel(&point_1, 'X');
-    engine.put_pixel(&point_2, 'Y');
-    engine.draw();
+    wait_key();
 
-    println!("\n\nPress a key to start");
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer);
     let mut triangle_1 = Triangle::new(Vec2::new(1, 3), Vec2::new(1, 8), Vec2::new(20, 20));
     let mut triangle_2 = Triangle::new(Vec2::new(100, 1), Vec2::new(80, 5), Vec2::new(90, 10));
     println!("Try triangle: {:?}", triangle_1);
-    for frame in 1..50 {
+    for _frame in 1..50 {
         engine.clear(' ');
         triangle_1.update_p2(triangle_1.p2.x+1, triangle_1.p2.y);
-        triangle_2.update_p2(triangle_2.p2.x, triangle_2.p2.y.saturating_sub(1));
+        triangle_2.update_p2(triangle_2.p2.x.saturating_sub(1), triangle_2.p2.y);
         engine.put_triangle(&triangle_1, 'O');
         engine.put_triangle(&triangle_2, '@');
         engine.draw();
-        sleep(time::Duration::from_millis(100))
+        sleep(time::Duration::from_millis(10))
     }
+}
+
+fn wait_key () {
+    println!("\n\nPress a key to start");
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer);
 }
