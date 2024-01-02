39ecc67cbb8d858abed922f3ea25ebfa2505106b
haingue <fabien.haingue@gmail.com>
2024-01-02 20:25:38 +0100

    FEAT: add 3d function

---
+++ src/core/math/vector.rs
@@ -0,0 +1,132 @@
+use std::ops;
+
+use crate::core::engine::Engine;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vec2 {
+  pub x: f32,
+  pub y: f32
+}
+
+impl ops::Mul<f32> for Vec2 {
+  type Output = Vec2;
+  fn mul(self, rhs: f32) -> Self::Output {
+    Vec2 {
+      x: self.x * rhs,
+      y: self.y * rhs
+    }
+  }
+}
+impl ops::Mul<Vec2> for f32 {
+  type Output = Vec2;
+  fn mul(self, v_rhs: Vec2) -> Self::Output {
+    Vec2 {
+      x: self * v_rhs.x,
+      y: self * v_rhs.y
+    }
+  }
+}
+impl ops::Div<f32> for Vec2 {
+  type Output = Vec2;
+  fn div(self, rhs: f32) -> Self::Output {
+    Vec2 {
+      x: self.x / rhs,
+      y: self.y / rhs
+    }
+  }
+}
+impl ops::Add<Vec2> for Vec2 {
+  type Output = Vec2;
+  fn add(self, v_rhs: Vec2) -> Vec2 {
+    Vec2 {
+      x: self.x + v_rhs.x,
+      y: self.y + v_rhs.y
+    }
+  }
+}
+
+impl Vec2 {
+  pub fn new (x: f32, y:f32) -> Vec2 {
+    Vec2 { x, y }
+  }
+  pub fn toScreen (&mut self, engine: &Engine) -> Vec2 {
+    let height = engine.height as f32;
+   let width = engine.width as f32;
+    Vec2 {
+      x: (height/width * self.x + 1.0) * (width as f32) / 2.0,
+      y: (-self.y + 1.0) * (height as f32) / 2.0
+     }
+  }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vec3 {
+  pub x: f32,
+  pub y: f32,
+  pub z: f32
+}
+impl ops::Mul<f32> for Vec3 {
+  type Output = Vec3;
+  fn mul(self, rhs: f32) -> Self::Output {
+    Vec3 {
+      x: self.x * rhs,
+      y: self.y * rhs,
+      z: self.z * rhs
+    }
+  }
+}
+impl ops::Mul<Vec3> for f32 {
+  type Output = Vec3;
+  fn mul(self, v_rhs: Vec3) -> Self::Output {
+    Vec3 {
+      x: self * v_rhs.x,
+      y: self * v_rhs.y,
+      z: self * v_rhs.z
+    }
+  }
+}
+impl ops::Div<f32> for Vec3 {
+  type Output = Vec3;
+  fn div(self, rhs: f32) -> Self::Output {
+    Vec3 {
+      x: self.x / rhs,
+      y: self.y / rhs,
+      z: self.z / rhs
+    }
+  }
+}
+impl ops::Add<Vec3> for Vec3 {
+  type Output = Vec3;
+  fn add(self, v_rhs: Vec3) -> Vec3 {
+    Vec3 {
+      x: self.x + v_rhs.x,
+      y: self.y + v_rhs.y,
+      z: self.z + v_rhs.z
+    }
+  }
+}
+
+impl Vec3 {
+  pub fn new (x: f32, y:f32, z:f32) -> Vec3 {
+    Vec3 { x, y, z }
+  }
+
+  pub fn projection (&mut self, focal_length: f32) -> Vec2 {
+    return focal_length * Vec2 {
+      x: self.x,
+      y: self.y
+    } / self.z
+  }
+  
+  pub fn rotation_x (&mut self, pitch:f32) -> Vec3 {
+    let y = f32::cos(pitch) * self.y - f32::sin(pitch) * self.z;
+    let z = f32::sin(pitch) * self.y + f32::cos(pitch) * self.z;
+    Vec3 {x: self.x, y, z}
+  }
+
+  pub fn rotation_y (&mut self, yaw:f32) -> Vec3 {
+    let x = f32::cos(yaw) * self.x + f32::sin(yaw) * self.z;
+    let z = -f32::sin(yaw) * self.x + f32::cos(yaw) * self.z;
+    Vec3 {x, y: self.y, z}
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod core;
 pub mod tools;
 
+<<<<<<< HEAD
 use std::time::Duration;
 use std::{default, io, time};
 use std::thread::sleep;
@@ -9,6 +10,16 @@ use crossterm::event::{poll, read, Event, KeyCode};
 
 use crate::core::engine::Engine;
 use crate::core::math::{Vec2, Triangle};
+=======
+use std::sync::mpsc::Receiver;
+use std::{io, time, pin};
+use std::thread::sleep;
+
+use crate::core::engine::{Engine, Camera};
+use crate::core::math::triangle::Triangle3D;
+use crate::core::math::vector::Vec3;
+use crate::core::math::{vector::Vec2, triangle::Triangle2D};
+>>>>>>> 872ccc8 (FEAT: add 3d function)
 use crate::tools::configuration::{Configuration};
 
 fn main() {
@@ -17,16 +28,23 @@ fn main() {
     println!("Configuration: {:?}", configuration);
     let mut engine: Engine = Engine::new(configuration.width, configuration.height - 1);
     println!("Engine: {:?}", engine.to_string());
+   
+    // demo_1(&mut engine);
+    // demo_2(&mut engine);
+    // demo_3(&mut engine);
+    // demo_4(&mut engine);
+    demo_5(&mut engine);
+}
 
-    wait_key();
-
-    let mut triangle_1 = Triangle::new(Vec2::new(1, 3), Vec2::new(1, 8), Vec2::new(20, 20));
-    let mut triangle_2 = Triangle::new(Vec2::new(100, 1), Vec2::new(80, 5), Vec2::new(90, 10));
+fn demo_1 (engine: &mut Engine) {
+    engine.wait_key();
+    let mut triangle_1 = Triangle2D::new(Vec2::new(1.0, 3.0), Vec2::new(1.0, 8.0), Vec2::new(20.0, 20.0));
+    let mut triangle_2 = Triangle2D::new(Vec2::new(100.0, 1.0), Vec2::new(80.0, 5.0), Vec2::new(90.0, 10.0));
     println!("Try triangle: {:?}", triangle_1);
     for _frame in 1..50 {
         engine.clear(' ');
-        triangle_1.update_p2(triangle_1.p2.x+1, triangle_1.p2.y);
-        triangle_2.update_p2(triangle_2.p2.x.saturating_sub(1), triangle_2.p2.y);
+        triangle_1.update_v2(triangle_1.v2.x+1.0, triangle_1.v2.y);
+        triangle_2.update_v2(triangle_2.v2.x.ceil(), triangle_2.v2.y);
         engine.put_triangle(&triangle_1, 'O');
         engine.put_triangle(&triangle_2, '@');
         engine.draw();
@@ -35,6 +53,72 @@ fn main() {
 
     // loop_print_events();
     return
+}
+
+fn demo_2 (engine: &mut Engine) {
+    let mut triangle_1 = Triangle2D::new(
+        Vec2::new( -0.5, -0.5),
+        Vec2::new(0.0, 0.5),
+        Vec2::new(0.5, -0.5));
+    println!("Try triangle: {:?}", triangle_1);
+    loop {
+        engine.clear(' ');
+        engine.put_triangle(&triangle_1.toScreen(&engine), '@');
+        engine.draw();
+        sleep(time::Duration::from_millis(10))
+    }
+}
+
+fn demo_3 (engine: &mut Engine) {
+    let mut triangle_1 = Triangle3D::new(
+        Vec3::new( -0.5,    -0.5,   1.0),
+        Vec3::new(  0.0,    0.5,    1.0),
+        Vec3::new(  0.5,    -0.5,   1.0));
+    println!("Try triangle: {:?}", triangle_1);
+    for _frame in 1..10 {
+        engine.clear(' ');
+        engine.put_triangle(&triangle_1.projection(1.0).toScreen(&engine), '@');
+        engine.draw();
+        triangle_1.translate(Vec3{x:0.5, y:0.5, z:0.5});
+        sleep(time::Duration::from_millis(100))
+    }
+}
+
+fn demo_4 (engine: &mut Engine) {
+    let mut triangle_1 = Triangle3D::new(
+        Vec3::new( -0.5,    -0.5,   0.50),
+        Vec3::new(  0.0,    0.5,    0.50),
+        Vec3::new(  0.5,    -0.5,   0.50));
+    println!("Try triangle: {:?}", triangle_1);
+    let mut t: f32 = 0.0;
+    loop {
+        engine.clear(' ');
+        t += 0.01;
+        engine.put_triangle(&triangle_1
+            .rotation_y(t)
+            .translate(Vec3{x:0.0, y:0.0, z:2.0})
+            .projection(1.0)
+            .toScreen(&engine), '@');
+        engine.draw();
+        //sleep(time::Duration::from_millis(100))
+    }
+}
+
+fn demo_5 (engine: &mut Engine) {
+    let mut mesh = vec![];
+    let mut triangle_1 = Triangle3D::new(
+        Vec3::new( -0.5,    -0.5,   0.50),
+        Vec3::new(  0.0,    0.5,    0.50),
+        Vec3::new(  0.5,    -0.5,   0.50));
+    mesh.push(triangle_1);
+    println!("Try triangle: {:?}", triangle_1);
+    let cam = Camera { position: Vec3 {x: 0.0, y:0.0, z: 0.0 }, pitch: 0.0, yaw: 0.0, focal_length: 1.0 };
+    loop {
+        engine.clear(' ');
+        engine.put_mesh(&mesh, &cam);
+        engine.draw();
+        //sleep(time::Duration::from_millis(100))
+    }
 }
 
 fn wait_key () -> io::Result<()> {
